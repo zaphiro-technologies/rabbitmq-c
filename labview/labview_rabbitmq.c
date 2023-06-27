c51af8bfa4786548019ddf199eaab0aef357a12a
Łukasz Kwiatkowski <lukikwiatkowski@gmail.com>
2023-06-27 16:33:37 +0200

    overall cleanup

--- labview/labview_rabbitmq.c
+++ labview/labview_rabbitmq.c
@@ -1,6 +1,5 @@
 #include <rabbitmq-c/amqp.h>
 #include <rabbitmq-c/tcp_socket.h>
-//#include <amqp_private.h>
 #include <amqp_framing.h>
 #include <string.h>
 #include "utils.h"
@@ -9,12 +8,8 @@
 #include <assert.h>
 #include <sys/time.h>
 
-enum ERROR_CODE {
-    RED,
-    GREEN,
-    BLUE
-};
-int const SIZE_OF_LABVIEW_CREATED_ERROR_STRING = 100;
+/* Cintools: Handle "cnt" field is int32 so max length is INT32_MAX. */
+#define MaxHandleStringLength INT32_MAX
 
 typedef enum error_code_enum_ {
 	// Following codes are copied from amqp_response_type_enum_ and are used by 
@@ -27,12 +22,38 @@ typedef enum error_code_enum_ {
                                       error, check replay */
 
 	// Next error codes are custom for this library	
-	_LABVIEW_ERROR_STR_LENGHT, /**< LabVIEW provided error string is not the correct lenght */
+	_STR_LEN_OVER_INT32MAX, /**< LabVIEW provided error string is not the correct lenght */
 	_CREATING_TCP_SOCKET, /**< Creating TCP socked failed */
 	_OPENING_TCP_SOCKET, /**< Openning TCP socked failed */
-	_OUT_OF_MEMORY /**< Copy function failed*/
+	_OUT_OF_MEMORY, /**< Copy function failed*/
+	_LSTRHANDLE_IS_NULL /**< LStrHandle is NULL */
 									   
 };
+
+int lv_strncpy(LStrHandle dest, const char *src) {
+	size_t len = strlen(src);
+	if (len > INT32_MAX) {
+		return _STR_LEN_OVER_INT32MAX;
+	}
+	if ( !DSCheckHandle( dest )) {
+		if (( *dest )->cnt < len ) {
+		/* Adjust the handle size to source string lenght */
+		DSSetHandleSize(( void* )dest, len);
+		}
+		if ( !DSCheckHandle( dest )) {
+			strncpy((*dest)->str, src, len);
+			(*dest)->cnt = len;
+		}
+		else {
+			return _OUT_OF_MEMORY;
+		}
+		return 0;
+	}
+	else {
+		return _LSTRHANDLE_IS_NULL;
+	}
+
+}
 
 LABVIEW_PUBLIC_FUNCTION
 char lv_rabbitmq_version(void) {
@@ -40,35 +61,26 @@ char lv_rabbitmq_version(void) {
 	return VERSION;
 }
 
-
-amqp_bytes_t queuename;
-struct lv_timeval {
-	long    tv_sec;
-	long	tv_usec;
-};
+int const MAX_ERROR_DESCRIPTION_LENGTH = 1000;
 
 /* This function is a modified version of the `die_on_amqp_error` function used in examples,
 enhanced with LabVIEW string support.*/
 LABVIEW_PUBLIC_FUNCTION
-int lv_report_amqp_error(amqp_rpc_reply_t x, char const *context, char *labview_error_string) {
-
-
-	//Check if the string is the correct size
-	char buffer[SIZE_OF_LABVIEW_CREATED_ERROR_STRING];
-	if (strlen(labview_error_string) != SIZE_OF_LABVIEW_CREATED_ERROR_STRING) {
-		return _LABVIEW_ERROR_STR_LENGHT;
-	}
+int lv_report_amqp_error(amqp_rpc_reply_t x, char const *context, LStrHandle error_description) {
+	char temp_str[MAX_ERROR_DESCRIPTION_LENGTH];
 
 	switch (x.reply_type) {
 		case AMQP_RESPONSE_NORMAL:
 			return _AMQP_RESPONSE_NORMAL;
 
 		case AMQP_RESPONSE_NONE:
-			sprintf(labview_error_string, "%s: missing RPC reply type!\n", context);
+			snprintf(temp_str, MAX_ERROR_DESCRIPTION_LENGTH, "%s: missing RPC reply type!\n", context);
+			lv_strncpy(error_description, temp_str);
 			return _AMQP_RESPONSE_NONE;
 
 		case AMQP_RESPONSE_LIBRARY_EXCEPTION:
-			sprintf(labview_error_string, "%s: %s\n", context, amqp_error_string2(x.library_error));
+			snprintf(temp_str, MAX_ERROR_DESCRIPTION_LENGTH, "%s: %s\n", context, amqp_error_string2(x.library_error));
+			lv_strncpy(error_description, temp_str);
 			return _AMQP_RESPONSE_LIBRARY_EXCEPTION;
 
 		case AMQP_RESPONSE_SERVER_EXCEPTION:
@@ -76,21 +88,24 @@ int lv_report_amqp_error(amqp_rpc_reply_t x, char const *context, char *labview_
 			case AMQP_CONNECTION_CLOSE_METHOD: {
 			amqp_connection_close_t *m =
 				(amqp_connection_close_t *)x.reply.decoded;
-			sprintf(labview_error_string, "%s: server connection error %uh, message: %.*s\n",
+			snprintf(temp_str, MAX_ERROR_DESCRIPTION_LENGTH, "%s: server connection error %uh, message: %.*s\n",
 					context, m->reply_code, (int)m->reply_text.len,
 					(char *)m->reply_text.bytes);
+			lv_strncpy(error_description, temp_str);
 			return  _AMQP_RESPONSE_SERVER_EXCEPTION;
 			}
 			case AMQP_CHANNEL_CLOSE_METHOD: {
 			amqp_channel_close_t *m = (amqp_channel_close_t *)x.reply.decoded;
-			sprintf(labview_error_string, "%s: server channel error %uh, message: %.*s\n",
+			snprintf(temp_str, MAX_ERROR_DESCRIPTION_LENGTH, "%s: server channel error %uh, message: %.*s\n",
 					context, m->reply_code, (int)m->reply_text.len,
 					(char *)m->reply_text.bytes);
+			lv_strncpy(error_description, temp_str);
 			return _AMQP_RESPONSE_SERVER_EXCEPTION;
 			}
 			default:
-			sprintf(labview_error_string, "%s: unknown server error, method id 0x%08X\n",
+			snprintf(temp_str, MAX_ERROR_DESCRIPTION_LENGTH, "%s: unknown server error, method id 0x%08X\n",
 					context, x.reply.id);
+			lv_strncpy(error_description, temp_str);
 			return _AMQP_RESPONSE_SERVER_EXCEPTION;
 		}
 		break;
@@ -98,105 +113,6 @@ int lv_report_amqp_error(amqp_rpc_reply_t x, char const *context, char *labview_
 }
 
 
-int lv_die_on_amqp_error(amqp_rpc_reply_t x, char const *context) {
-	switch (x.reply_type) {
-	case AMQP_RESPONSE_NORMAL:
-		return 1;
-
-	case AMQP_RESPONSE_NONE:
-		return 2;
-
-	case AMQP_RESPONSE_LIBRARY_EXCEPTION:
-		return 3;
-
-	case AMQP_RESPONSE_SERVER_EXCEPTION:
-		switch (x.reply.id) {
-		case AMQP_CONNECTION_CLOSE_METHOD: {
-			amqp_connection_close_t *m =
-				(amqp_connection_close_t *)x.reply.decoded;
-			return 4;
-		}
-		case AMQP_CHANNEL_CLOSE_METHOD: {
-			amqp_channel_close_t *m = (amqp_channel_close_t *)x.reply.decoded;
-			return 5;
-		}
-		default:
-			return 6;
-		}
-		break;
-	}
-
-}
-
-
-char *lv_die_on_amqp_error2(amqp_rpc_reply_t x, char const *context, char *des) {
-
-	char *r1 = "1";
-	char *r2 = "2";
-	char *r3 = "3";
-	char *r4 = "4";
-	char *r5 = "5";
-	char *r6 = "6";
-
-	switch (x.reply_type) {
-	case AMQP_RESPONSE_NORMAL:
-		des = r1;
-		return des;
-
-	case AMQP_RESPONSE_NONE:
-		des = r2;
-		return des;
-
-	case AMQP_RESPONSE_LIBRARY_EXCEPTION:
-		return amqp_error_string2(x.library_error);
-
-	case AMQP_RESPONSE_SERVER_EXCEPTION:
-		switch (x.reply.id) {
-		case AMQP_CONNECTION_CLOSE_METHOD: {
-			amqp_connection_close_t *m =
-				(amqp_connection_close_t *)x.reply.decoded;
-			des = r4;
-			return des;
-		}
-		case AMQP_CHANNEL_CLOSE_METHOD: {
-			amqp_channel_close_t *m = (amqp_channel_close_t *)x.reply.decoded;
-			des = r5;
-			return des;
-		}
-		default:
-			des = r6;
-			return des;
-		}
-		break;
-	}
-
-}
-
-
-
-int lv_amqp_dump(size_t len) {
-	int count = 0;
-	size_t i;
-
-	for (i = 0; i < len; i++) {
-		count++;
-	}
-	return count;
-}
-
-
-
-
-
-
-struct amqp_connection_state_t* lv_int_to_pointer(int *conn_int) {
-	amqp_connection_state_t *x;
-	x = (amqp_connection_state_t*)conn_int;
-	return *x;
-}
-
-
-//Fixed
 LABVIEW_PUBLIC_FUNCTION
 int64_t lv_amqp_new_connection() {
 	// conn is an opaque struct pointer
@@ -208,18 +124,14 @@ int64_t lv_amqp_new_connection() {
 }
 
 
-
-//Reviewed
 LABVIEW_PUBLIC_FUNCTION
-int lv_amqp_close_connection(int64_t conn_intptr) {
+int lv_amqp_close_connection(int64_t conn_intptr, LStrHandle error_description) {
 	// cast back to amqp_connection_state_t opaque struct pointer
 	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
-	int r = lv_die_on_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS),"Connection close");
-	return r;
+	return lv_report_amqp_error(amqp_connection_close(conn, AMQP_REPLY_SUCCESS),"Connection close", error_description);
 }
 
 
-//Reviewed
 LABVIEW_PUBLIC_FUNCTION
 void lv_amqp_destroy_connection(int64_t conn_intptr) {
 	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
@@ -227,38 +139,23 @@ void lv_amqp_destroy_connection(int64_t conn_intptr) {
 }
 
 
-//ok
 LABVIEW_PUBLIC_FUNCTION
-void lv_amqp_channel_open(int64_t conn_intptr, uint16_t  channel, char *labview_error_string) {
+void lv_amqp_channel_open(int64_t conn_intptr, uint16_t  channel, LStrHandle error_description) {
 	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
 	amqp_channel_open(conn, channel);
-	return lv_report_amqp_error(amqp_get_rpc_reply(conn), "Opening channel", labview_error_string);
-
+	return lv_report_amqp_error(amqp_get_rpc_reply(conn), "Opening channel", error_description);
 }
 
 
-//Reviewed
 LABVIEW_PUBLIC_FUNCTION
-int lv_amqp_channel_close(int64_t conn_intptr) {
+int lv_amqp_channel_close(int64_t conn_intptr, uint16_t  channel, LStrHandle error_description) {
 	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
-	int r = lv_die_on_amqp_error(amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS), "Closing channel");
-	return r;
-}
-// TO DO: channel is set constant to 1, should be a parameter
-
-
-// This functions should be removed
-LABVIEW_PUBLIC_FUNCTION
-int lv_amqp_get_rpc_reply(int64_t conn_intptr, char *operation) {
-	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
-        int r =	lv_die_on_amqp_error(amqp_get_rpc_reply(conn), "aaa");
-       return r;
+	return lv_report_amqp_error(amqp_channel_close(conn, channel, AMQP_REPLY_SUCCESS), "Closing channel", error_description);
 }
 
 
-//ok
 LABVIEW_PUBLIC_FUNCTION
-int lv_amqp_exchange_declare(int64_t conn_intptr, uint16_t  channel, char *exchange, char *exchangetype, char *labview_error_string) {
+int lv_amqp_exchange_declare(int64_t conn_intptr, uint16_t  channel, char *exchange, char *exchangetype, LStrHandle error_description) {
 	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
 
 	amqp_boolean_t PASSIVE = 0;
@@ -269,22 +166,12 @@ int lv_amqp_exchange_declare(int64_t conn_intptr, uint16_t  channel, char *excha
 		amqp_cstring_bytes(exchangetype), PASSIVE, DURABLE, AUTO_DELETE, INTERNAL,
 		amqp_empty_table);
 
-    return lv_report_amqp_error(amqp_get_rpc_reply(conn), "Exchange declare", labview_error_string);
+    return lv_report_amqp_error(amqp_get_rpc_reply(conn), "Exchange declare", error_description);
 }
 
 
 LABVIEW_PUBLIC_FUNCTION
-char *name(char *res) {
-	char *char_temp = "hahaha2";
-	res = char_temp;
-	return res;
-
-};
-
-
-//OK
-LABVIEW_PUBLIC_FUNCTION
-int lv_amqp_login(int64_t conn_intptr, char *host, int port, int timeout_sec, char *username, char *password, char *labview_error_string) {
+int lv_amqp_login(int64_t conn_intptr, char *host, int port, int timeout_sec, char *username, char *password, LStrHandle error_description) {
 	
 	int status;
 	amqp_socket_t *socket = NULL;
@@ -312,15 +199,10 @@ int lv_amqp_login(int64_t conn_intptr, char *host, int port, int timeout_sec, ch
 									// 4096 is the minimum size, 2^31-1 is the maximum, a good default is 131072 (128KB),
 	int const HEARTBEAT = 0; 		// the number of seconds between heartbeat frames to request of the broker. A value of 0 disables heartbeats.
 
-	return lv_report_amqp_error(amqp_login(conn, VHOST, CHANNEL_MAX, FRAME_MAX, HEARTBEAT, AMQP_SASL_METHOD_PLAIN, username, password), "Logging in", labview_error_string);	
+	return lv_report_amqp_error(amqp_login(conn, VHOST, CHANNEL_MAX, FRAME_MAX, HEARTBEAT, AMQP_SASL_METHOD_PLAIN, username, password), "Logging in", error_description);	
 }
 
 
-
-
-
-
-//Reviewed
 LABVIEW_PUBLIC_FUNCTION
 void lv_amqp_basic_publish(int64_t conn_intptr, char *exchange, char *routingkey, char *messagebody) {
 	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
@@ -336,15 +218,8 @@ void lv_amqp_basic_publish(int64_t conn_intptr, char *exchange, char *routingkey
 }
 
 
-
-
-
-
-
-
-//ok
 LABVIEW_PUBLIC_FUNCTION
-int lv_amqp_create_queue(int64_t conn_intptr, uint16_t  channel, char *exchange, char *bindingkey, char *labview_error_string) {
+int lv_amqp_create_queue(int64_t conn_intptr, uint16_t  channel, char *exchange, char *bindingkey, LStrHandle error_description) {
 	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
 	int status;
 	amqp_bytes_t queuename;
@@ -355,19 +230,19 @@ int lv_amqp_create_queue(int64_t conn_intptr, uint16_t  channel, char *exchange,
 	amqp_boolean_t AUTO_DELETE = 1;
 	amqp_queue_declare_ok_t *r = amqp_queue_declare(conn, channel, amqp_empty_bytes, PASSIVE, DURABLE, EXCLUSIVE, AUTO_DELETE, amqp_empty_table);
 
-    status = lv_report_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue", labview_error_string);
+    status = lv_report_amqp_error(amqp_get_rpc_reply(conn), "Declaring queue", error_description);
 	if (status != 1) {
 		return status;
 	}
 	
 	queuename = amqp_bytes_malloc_dup(r->queue);
 	if (queuename.bytes == NULL) {
-		strncpy(labview_error_string, "Out of memory while copying queue name", SIZE_OF_LABVIEW_CREATED_ERROR_STRING);
+		lv_strncpy(error_description, "Out of memory while copying queue name");
 		return _OUT_OF_MEMORY;
 	}
 
 	amqp_queue_bind(conn, channel, queuename, amqp_cstring_bytes(exchange), amqp_cstring_bytes(bindingkey), amqp_empty_table);
-	status = lv_report_amqp_error(amqp_get_rpc_reply(conn), "Binding queue", labview_error_string);
+	status = lv_report_amqp_error(amqp_get_rpc_reply(conn), "Binding queue", error_description);
 	if (status != 1) {
 		return status;
 	}
@@ -378,15 +253,13 @@ int lv_amqp_create_queue(int64_t conn_intptr, uint16_t  channel, char *exchange,
 	amqp_basic_consume(conn, channel, queuename, amqp_empty_bytes, NO_LOCAL, NO_ACK, EXCLUSIVE2, amqp_empty_table);
 	/* amqp_basic_consume is used to register a consumer on the queue,
 	 so that the broker will start delivering messages to it.*/
-	status = lv_report_amqp_error(amqp_get_rpc_reply(conn), "Basic consume", labview_error_string);
+	status = lv_report_amqp_error(amqp_get_rpc_reply(conn), "Basic consume", error_description);
     return status;
 }
 
 
-
-//Reviewed
 LABVIEW_PUBLIC_FUNCTION
-int lv_amqp_consume_message(int64_t conn_intptr, int timeout_sec, LStrHandle output, char *labview_error_string) {
+int lv_amqp_consume_message(int64_t conn_intptr, int timeout_sec, LStrHandle output, LStrHandle error_description) {
 	amqp_connection_state_t conn = (amqp_connection_state_t)conn_intptr;
 
 	int status;
@@ -398,125 +271,16 @@ int lv_amqp_consume_message(int64_t conn_intptr, int timeout_sec, LStrHandle out
 
 	amqp_maybe_release_buffers(conn);
 
-	status = lv_report_amqp_error(amqp_consume_message(conn, &envelope, &tval, 0),"Consuming message", labview_error_string);
+	status = lv_report_amqp_error(amqp_consume_message(conn, &envelope, &tval, 0),"Consuming message", error_description);
 	if (status != 1) {
 		return status;
 	}
 
 	if (envelope.message.body.len > 0) {
-		if ( !DSCheckHandle( output )) {
-			if (( *output )->cnt < envelope.message.body.len ) {
-			/* Adjust the handle size to message body lenght */
-			DSSetHandleSize(( void* )output, envelope.message.body.len );
-			}
-			if ( !DSCheckHandle( output )) {
-				strncpy((*output)->str, envelope.message.body.bytes, envelope.message.body.len);
-				(*output)->cnt = envelope.message.body.len;
-			}
-		}
+		lv_strncpy(output, envelope.message.body.bytes);
 	}
 
 	amqp_destroy_envelope(&envelope);
 	return status;
 }
-
-
-
-LABVIEW_PUBLIC_FUNCTION
-void lv_amqp_destroy_envelope(int envelope) {
-	//amqp_envelope_t *x;
-	//x = (amqp_envelope_t*)envelope;
-	amqp_destroy_envelope(envelope);
-}
-
-
-// Following function are not used in the code 
-
-/* Handle "cnt" field is int32 so max length is INT32_MAX. */
-#define MaxHandleStringLength INT32_MAX
-
-
-LABVIEW_PUBLIC_FUNCTION
-void LVLStrHandle(int *conn_int, int *number, LStrHandle input, LStrHandle output)
-{
-	int32 len;
-	uInt32 i, j, stringLength;
-	uInt32 temp;
-	temp = 50;
-
-
-	char  *aaa;
-	amqp_connection_state_t *x;
-	x = (amqp_connection_state_t*)conn_int;
-	//设置获取数据的超时时间
-	struct lv_timeval lv_timeval1;
-	lv_timeval1.tv_sec = 0;
-	lv_timeval1.tv_usec = 100000;
-
-	amqp_rpc_reply_t res;
-	amqp_envelope_t envelope;
-	amqp_maybe_release_buffers(*x);
-	res = amqp_consume_message(*x, &envelope, &lv_timeval1, 0);
-
-	if (envelope.message.body.len > 0) {
-		*number = lv_amqp_dump(envelope.message.body.len);
-
-		//amqp_destroy_envelope(&envelope);
-		aaa = envelope.message.body.bytes;
-
-		//return envelope.message.body.bytes;
-
-
-
-
-
-
-		len = (*input)->cnt;
-		/* If we really have a handle, make sure it has enough room.. */
-		
-			stringLength = len * 2;
-			if (stringLength > MaxHandleStringLength) {
-				stringLength = MaxHandleStringLength;
-			}
-
-			
-				/* Adjust the handle size to hold everything we want to put in it */
-			
-			
-
-			
-				strncpy((*output)->str, (*input)->str, len);
-				/* Reverse "i" characters. */
-				/*
-				for ( i = len, j = 0; ( i > 0 ) && ( j < ( stringLength - len )); j++ )
-				{
-				(*output)->str[j + len] = (*input)->str[--i];
-				}
-				*/
-				char *bbb = "labview";
-				//bbb = envelope.message.body.bytes;
-				int l = strlen(bbb);
-				if (l > 0) {
-					for (i = 0; i < l; i++) {
-						(*output)->str[i] = bbb[i];
-					}
-				}
-				(*output)->cnt = l;
-			
-		
-
-
-
-	}
-	else {
-            int fff;
-            fff = 0;
-	}
-	
-	
-	//amqp_destroy_envelope(&envelope);
-
-
-}
-
 
